80c9d6a1f51ccb61c9675595499e8d37383dd070
thundax-lyp <731378491@qq.com>
2026-04-27 12:31:16 +0800

    Refactor(assist): 收敛异步任务持久化表达

--- sandwish-infra/src/main/java/com/github/thundax/modules/assist/persistence/dataobject/AsyncTaskDO.java
+++ sandwish-infra/src/main/java/com/github/thundax/modules/assist/persistence/dataobject/AsyncTaskDO.java
@@ -1,13 +1,17 @@
 package com.github.thundax.modules.assist.persistence.dataobject;
 
-import com.github.thundax.common.persistence.AdminDataEntity;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 /**
  * 异步任务持久化对象。
  */
 @NoArgsConstructor
-public class AsyncTaskDO extends AdminDataEntity<AsyncTaskDO> {
+public class AsyncTaskDO {
+
+    private String id;
+    private boolean isNewRecord;
 
     private String title;
     private String status;
@@ -15,9 +19,32 @@ public class AsyncTaskDO extends AdminDataEntity<AsyncTaskDO> {
     private String data;
     private Boolean isPrivate;
     private Integer expiredSeconds;
+    private Integer priority;
+    private String remarks;
+    private Date createDate;
+    private String createUserId;
+    private Date updateDate;
+    private String updateUserId;
+    private String delFlag;
 
     public AsyncTaskDO(String id) {
-        super(id);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean getIsNewRecord() {
+        return isNewRecord;
+    }
+
+    public void setIsNewRecord(boolean isNewRecord) {
+        this.isNewRecord = isNewRecord;
     }
 
     public String getTitle() {
@@ -66,5 +93,61 @@ public class AsyncTaskDO extends AdminDataEntity<AsyncTaskDO> {
 
     public void setExpiredSeconds(Integer expiredSeconds) {
         this.expiredSeconds = expiredSeconds;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(String updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
     }
 }

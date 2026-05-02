26d3443d593ec4d093eb8c1c7356c7fcd86351be
thundax-lyp <731378491@qq.com>
2026-05-02 21:47:38 +0800

    Docs(service): 清理重复接口注释

--- sandwish-biz/src/main/java/com/github/thundax/modules/assist/service/AsyncTaskService.java
+++ sandwish-biz/src/main/java/com/github/thundax/modules/assist/service/AsyncTaskService.java
@@ -5,32 +5,11 @@ import com.github.thundax.modules.assist.entity.AsyncTask;
 
 public interface AsyncTaskService {
 
-    /**
-     * 获取
-     *
-     * @param id id
-     * @return 对象
-     */
     AsyncTask getById(EntityId id);
 
-    /**
-     * 新增
-     *
-     * @param asyncTask asyncTask
-     */
     void add(AsyncTask asyncTask);
 
-    /**
-     * 更新
-     *
-     * @param asyncTask asyncTask
-     */
     void update(AsyncTask asyncTask);
 
-    /**
-     * 删除
-     *
-     * @param id id
-     */
     void deleteById(EntityId id);
 }
--- sandwish-common/sandwish-common-mybatis/src/main/java/com/github/thundax/common/service/CrudService.java
+++ sandwish-common/sandwish-common-mybatis/src/main/java/com/github/thundax/common/service/CrudService.java
@@ -11,121 +11,33 @@ import java.util.List;
  */
 public interface CrudService<T> extends BaseService {
 
-    /**
-     * 对象类型
-     *
-     * @return 对象类型
-     */
     Class<T> getElementType();
 
-    /**
-     * 新对象
-     *
-     * @param entity 对象
-     * @return 对象
-     */
     T newEntity(String id);
 
-    /**
-     * 获取对象
-     *
-     * @param id id
-     * @return 对象
-     */
     T getById(T entity);
 
-    /**
-     * 获取对象
-     *
-     * @param id id
-     * @return 对象
-     */
     T getById(EntityId id);
 
-    /**
-     * 根据id获取多条数据
-     *
-     * @param ids id list
-     * @return 对象列表
-     */
     List<T> batchGetByIds(List<EntityId> ids);
 
-    /**
-     * 获取列表
-     *
-     * @param entity 查询条件
-     * @return 列表
-     */
     List<T> list(T entity);
 
-    /**
-     * 获取对象
-     *
-     * @param entity 查询条件
-     * @return 对象
-     */
     T getOne(T entity);
 
-    /**
-     * 获取分页数据
-     *
-     * @param entity 查询条件
-     * @param page 分页
-     * @return 分页
-     */
     Page<T> page(T entity, Page<T> page);
 
-    /**
-     * 获取 count
-     *
-     * @param entity 查询条件
-     * @return count
-     */
     long count(T entity);
 
-    /**
-     * 新增
-     *
-     * @param entity 对象
-     */
     void add(T entity);
 
-    /**
-     * 更新
-     *
-     * @param entity 对象
-     */
     void update(T entity);
 
-    /**
-     * 删除
-     *
-     * @param entity 对象
-     * @return 影响记录数
-     */
     int deleteById(EntityId id);
 
-    /**
-     * 删除
-     *
-     * @param ids id list
-     * @return 影响记录数
-     */
     int batchDeleteById(List<EntityId> ids);
 
-    /**
-     * 排序
-     *
-     * @param entity 对象
-     * @return 影响记录数
-     */
     int updatePriority(T entity);
 
-    /**
-     * 排序
-     *
-     * @param list 列表
-     * @return 影响记录数
-     */
     int updatePriority(List<T> list);
 }

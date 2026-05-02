59417e46332e83475358136131f250ef54c38b88
thundax-lyp <731378491@qq.com>
2026-05-02 22:03:51 +0800

    Refactor(storage): 下沉存储VO基础字段

--- sandwish-admin-api/src/main/java/com/github/thundax/modules/storage/vo/StorageVo.java
+++ sandwish-admin-api/src/main/java/com/github/thundax/modules/storage/vo/StorageVo.java
@@ -1,16 +1,27 @@
 package com.github.thundax.modules.storage.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.github.thundax.common.vo.BaseVo;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @ApiModel(value = "Storage", description = "存储资源")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class StorageVo extends BaseVo {
+public class StorageVo implements Serializable {
+
+    private String id;
+    private Integer priority;
+    private String remarks;
+
+    private Date createDate;
+    private Date updateDate;
 
     private String name;
     private String extendName;
@@ -28,12 +39,65 @@ public class StorageVo extends BaseVo {
 
     private String url;
 
-    public StorageVo() {
-        super();
-    }
+    public StorageVo() {}
 
     public StorageVo(String id) {
-        super(id);
+        this.id = id;
+    }
+
+    @ApiModelProperty(name = "id", value = "ID，唯一")
+    @JsonProperty("id")
+    @Size(max = 64, message = "ID长度不能超过64")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @ApiModelProperty(name = "priority", value = "排序数", example = "0")
+    @JsonProperty("priority")
+    @Min(value = 0, message = "\"排序数\"必须不能小于 0")
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    @ApiModelProperty(name = "remarks", value = "备注")
+    @JsonProperty("remarks")
+    @Size(max = 200, message = "\"备注\"长度不能超过 200")
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    @ApiModelProperty(name = "createDate", value = "创建时间")
+    @JsonProperty("createDate")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @ApiModelProperty(name = "updateDate", value = "修改时间")
+    @JsonProperty("updateDate")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
     }
 
     @ApiModelProperty(name = "name", value = "名称")

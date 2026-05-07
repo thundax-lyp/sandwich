9285607051cab97038808a9703ca4f29e5662a26
Thundax <thundax@qq.com>
2026-05-08 02:01:54 +0800

    Refactor(assist): 删除无用加密服务

--- sandwish-biz/src/main/java/com/github/thundax/modules/assist/service/impl/AbstractEncryptServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.github.thundax.modules.assist.service.impl;
-
-import com.github.thundax.modules.assist.service.EncryptService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public abstract class AbstractEncryptServiceImpl implements EncryptService {
-    protected final Logger logger = LoggerFactory.getLogger(getClass());
-}

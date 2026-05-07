420f9e58fc8401fc31b853f7a362c1185c13514a
Thundax <thundax@qq.com>
2026-05-07 23:34:31 +0800

    Test(common): 修复系统日志切面测试

--- sandwish-common/sandwish-common-log/src/test/java/com/github/thundax/common/log/aspect/SysLogAspectTest.java
+++ sandwish-common/sandwish-common-log/src/test/java/com/github/thundax/common/log/aspect/SysLogAspectTest.java
@@ -8,18 +8,18 @@ import static org.junit.Assert.assertTrue;
 import com.github.thundax.common.log.annotation.SysLog;
 import com.github.thundax.common.log.model.SysLogEvent;
 import com.github.thundax.common.log.producer.SysLogProducer;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 
 public class SysLogAspectTest {
 
     @Test
     public void shouldProduceSuccessEvent() throws Throwable {
-        SysLogProducer producer = Mockito.mock(SysLogProducer.class);
+        CapturingSysLogProducer producer = new CapturingSysLogProducer();
         SysLogAspect aspect = new SysLogAspect(producer);
         Method method = FixtureService.class.getMethod("success");
         ProceedingJoinPoint joinPoint = joinPoint(method, "ok", null);
@@ -27,7 +27,7 @@ public class SysLogAspectTest {
         Object result = aspect.around(joinPoint, method.getAnnotation(SysLog.class));
 
         assertEquals("ok", result);
-        SysLogEvent event = capturedEvent(producer);
+        SysLogEvent event = producer.event;
         assertEquals("create user", event.getAction());
         assertEquals("USER", event.getType());
         assertEquals(FixtureService.class.getName(), event.getClassName());
@@ -38,7 +38,7 @@ public class SysLogAspectTest {
 
     @Test
     public void shouldProduceFailedEvent() throws Throwable {
-        SysLogProducer producer = Mockito.mock(SysLogProducer.class);
+        CapturingSysLogProducer producer = new CapturingSysLogProducer();
         SysLogAspect aspect = new SysLogAspect(producer);
         Method method = FixtureService.class.getMethod("failure");
         RuntimeException failure = new RuntimeException("boom");
@@ -50,29 +50,86 @@ public class SysLogAspectTest {
             assertEquals(failure, ex);
         }
 
-        SysLogEvent event = capturedEvent(producer);
+        SysLogEvent event = producer.event;
         assertFalse(event.isSuccess());
         assertEquals("boom", event.getErrorMessage());
     }
 
-    private ProceedingJoinPoint joinPoint(Method method, Object result, Throwable failure) throws Throwable {
-        MethodSignature signature = Mockito.mock(MethodSignature.class);
-        Mockito.when(signature.getMethod()).thenReturn(method);
-        Mockito.when(signature.getDeclaringTypeName()).thenReturn(FixtureService.class.getName());
-        ProceedingJoinPoint joinPoint = Mockito.mock(ProceedingJoinPoint.class);
-        Mockito.when(joinPoint.getSignature()).thenReturn(signature);
-        if (failure == null) {
-            Mockito.when(joinPoint.proceed()).thenReturn(result);
-        } else {
-            Mockito.when(joinPoint.proceed()).thenThrow(failure);
-        }
-        return joinPoint;
+    private ProceedingJoinPoint joinPoint(final Method method, final Object result, final Throwable failure) {
+        final MethodSignature signature = methodSignature(method);
+        return (ProceedingJoinPoint) Proxy.newProxyInstance(
+                ProceedingJoinPoint.class.getClassLoader(),
+                new Class<?>[] {ProceedingJoinPoint.class},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method invokedMethod, Object[] args) throws Throwable {
+                        String methodName = invokedMethod.getName();
+                        if ("getSignature".equals(methodName)) {
+                            return signature;
+                        }
+                        if ("proceed".equals(methodName)) {
+                            if (failure != null) {
+                                throw failure;
+                            }
+                            return result;
+                        }
+                        if ("toString".equals(methodName)) {
+                            return "ProceedingJoinPointStub";
+                        }
+                        return defaultValue(invokedMethod.getReturnType());
+                    }
+                });
     }
 
-    private SysLogEvent capturedEvent(SysLogProducer producer) {
-        ArgumentCaptor<SysLogEvent> captor = ArgumentCaptor.forClass(SysLogEvent.class);
-        Mockito.verify(producer).produce(captor.capture());
-        return captor.getValue();
+    private MethodSignature methodSignature(final Method method) {
+        return (MethodSignature) Proxy.newProxyInstance(
+                MethodSignature.class.getClassLoader(),
+                new Class<?>[] {MethodSignature.class},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method invokedMethod, Object[] args) {
+                        String methodName = invokedMethod.getName();
+                        if ("getMethod".equals(methodName)) {
+                            return method;
+                        }
+                        if ("getDeclaringTypeName".equals(methodName)) {
+                            return FixtureService.class.getName();
+                        }
+                        if ("toString".equals(methodName)) {
+                            return "MethodSignatureStub";
+                        }
+                        return defaultValue(invokedMethod.getReturnType());
+                    }
+                });
+    }
+
+    private static Object defaultValue(Class<?> type) {
+        if (!type.isPrimitive()) {
+            return null;
+        }
+        if (Boolean.TYPE.equals(type)) {
+            return false;
+        }
+        if (Integer.TYPE.equals(type)) {
+            return 0;
+        }
+        if (Long.TYPE.equals(type)) {
+            return 0L;
+        }
+        if (Void.TYPE.equals(type)) {
+            return null;
+        }
+        return 0;
+    }
+
+    private static class CapturingSysLogProducer implements SysLogProducer {
+
+        private SysLogEvent event;
+
+        @Override
+        public void produce(SysLogEvent event) {
+            this.event = event;
+        }
     }
 
     private static class FixtureService {

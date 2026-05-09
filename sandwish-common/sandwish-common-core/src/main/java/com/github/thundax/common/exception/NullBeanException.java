db1362b19051f00912ae6823191a0ff0a311e0a3
Thundax <thundax@qq.com>
2026-05-10 01:11:50 +0800

    Fix(common): 支持领域ID异常参数

--- sandwish-common/sandwish-common-core/src/main/java/com/github/thundax/common/exception/NullBeanException.java
+++ sandwish-common/sandwish-common-core/src/main/java/com/github/thundax/common/exception/NullBeanException.java
@@ -1,11 +1,11 @@
 package com.github.thundax.common.exception;
 
 import com.github.thundax.common.i18n.I18nMessages;
-import com.github.thundax.common.id.EntityId;
+import com.github.thundax.common.id.BaseLongId;
 
 public class NullBeanException extends ApiException {
 
-    public NullBeanException(String name, EntityId id) {
+    public NullBeanException(String name, BaseLongId id) {
         super(I18nMessages.getMessage("common.exception.null-bean", name, id));
     }
 }

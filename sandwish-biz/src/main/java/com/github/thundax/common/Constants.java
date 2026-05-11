f8f9ed97c78d1eecdb3211248ca4cd829eeed830
thundax-lyp <731378491@qq.com>
2026-05-11 11:35:18 +0800

    Refactor(common): 对齐项目常量前缀

--- sandwish-biz/src/main/java/com/github/thundax/common/Constants.java
+++ sandwish-biz/src/main/java/com/github/thundax/common/Constants.java
@@ -2,11 +2,11 @@ package com.github.thundax.common;
 
 public class Constants {
 
-    public static final String CACHE_PREFIX = "_INTERACTION_";
+    public static final String CACHE_PREFIX = "_SANDWISH_";
 
-    public static final String QUEUE_PREFIX = "interaction.";
+    public static final String QUEUE_PREFIX = "sandwish.";
 
-    public static final String COOKIE_PREFIX = "interaction.";
+    public static final String COOKIE_PREFIX = "sandwish.";
 
     public static final String HEADER_TOKEN = "Access-Token";
     public static final String PARAM_TOKEN = "token";
